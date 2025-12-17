2e9a2dcbee659ceefa81d985761f49425e0bd054
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-16 16:42:55 -0800

    feat: Function that finds correct upper/lower case char code.

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -7,6 +7,36 @@
 //      c. Symbols should be untouched (i.e. "-", "_", "&", "$", etc.)
 // 3. Return joined array (into single string) of the shifted characters.
 
-const caesarCipher = (key, string) => {};
+const getShiftedCharCode = (key, code) => {
+  if (code >= 97 && code <= 122) {
+    for (let i = 1; i < key + 1; i++) {
+      code++;
+      if (code === 123) {
+        code = 97;
+      }
+    }
+  }
+
+  if (code >= 65 && code <= 90) {
+    for (let i = 1; i < key + 1; i++) {
+      code++;
+      if (code === 91) {
+        code = 65;
+      }
+    }
+  }
+
+  return code;
+};
+
+const caesarCipher = (key, string) => {
+  const stringArray = string.split("");
+
+  const shiftedStringArray = stringArray.map((char) => {
+    let code = char.charCodeAt(0);
+
+    //const updatedCode = getCharCode(key, code)
+  });
+};
 
 export default caesarCipher;
