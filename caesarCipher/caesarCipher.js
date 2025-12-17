74a1106a9f7fba6219f25e2a12c1f16599abe271
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-16 17:15:00 -0800

    Comment cleanup

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -1,12 +1,3 @@
-// 1. Split() word into array
-// 2. Map() through each character
-//      a. Using character codes and the given key, replace character
-//          with the correct character based on the given key (character code number is increased by the key)
-//      b. If character is at end of the alphabet (i.e. "z"), counting should
-//          wrap around to beginning of alphabet, and should keep same case (upper or lower).
-//      c. Symbols should be untouched (i.e. "-", "_", "&", "$", etc.)
-// 3. Return joined array (into single string) of the shifted characters.
-
 const getShiftedCharCode = (key, code) => {
   if (code >= 97 && code <= 122) {
     for (let i = 1; i < key + 1; i++) {
